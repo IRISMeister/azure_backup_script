3c9fd54844558ae79caab42aeae2183671c7e41b
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2019-06-27 18:21:18 +0900

    'initial'

---
+++ postScript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# variables used for returning the status of the script
+
+success=0
+error=1
+warning=2
+
+status=$success
+
+log_path="/etc/postScript.log"   #path of log file
+printf  "Logs:\n" > $log_path
+
+iris session iris1 -U%SYS "##class(Backup.General).ExternalThaw(0)"
+status=$?
+if [ $status -eq 5 ]; then
+echo "SYSTEM IS UNFROZEN"
+printf  "SYSTEM IS UNFROZEN\n" >> $log_path
+elif [ $status -eq 3 ]; then
+echo "SYSTEM UNFREEZE FAILED"
+printf  "SYSTEM UNFREEZE FAILED\n" >> $log_path
+status=$error
+fi
+
+exit $status
