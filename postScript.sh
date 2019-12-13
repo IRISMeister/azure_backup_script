676846ca034a4a8fbe231bce521a5998f8a3b395
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2019-12-13 16:20:51 +0900

    beautify

--- postScript.sh
+++ postScript.sh
@@ -4,7 +4,6 @@
 success=0
 error=1
 warning=2
-
 status=$success
 
 log_path="/var/tmp/postScript.log"   #path of log file
@@ -13,13 +12,13 @@ printf  "Logs:\n" > $log_path
 iris session iris -U%SYS "##class(Backup.General).ExternalThaw(0)"
 status=$?
 if [ $status -eq 5 ]; then
-echo "SYSTEM IS UNFROZEN"
-printf  "SYSTEM IS UNFROZEN\n" >> $log_path
-status=$success
+  echo "SYSTEM IS UNFROZEN"
+  printf  "SYSTEM IS UNFROZEN\n" >> $log_path
+  status=$success
 elif [ $status -eq 3 ]; then
-echo "SYSTEM UNFREEZE FAILED"
-printf  "SYSTEM UNFREEZE FAILED\n" >> $log_path
-status=$error
+  echo "SYSTEM UNFREEZE FAILED"
+  printf  "SYSTEM UNFREEZE FAILED\n" >> $log_path
+  status=$error
 fi
 
 exit $status
