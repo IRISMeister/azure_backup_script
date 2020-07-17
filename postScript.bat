bdbc0b2edec6ada4834d042003ddf7261d220782
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2020-07-17 18:31:21 +0900

    BAT

--- postScript.bat
+++ postScript.bat
@@ -1,1 +1,23 @@
+echo off
+SET success=0
+SET error=1
+SET warning=2
+SET status=%success%
+
+SET log_path="c:\temp/postScript.log"
+echo Logs: > %log_path%
+
 C:\InterSystems\IRIS\bin\iris runw iris THAW^^ZBACKUP %%SYS
+SET sts=%errorlevel%
+if %sts%==0 goto OK
+if %sts%==1 goto FAIL
+:OK
+echo SYSTEM IS UNFROZEN >> %log_path%
+SET status=%success%
+goto END
+:FAIL
+echo SYSTEM UNFREEZE FAILED >> %log_path%
+SET status=%error%
+:END
+
+exit/B %status%
--- preScript.bat
+++ preScript.bat
@@ -1,1 +1,25 @@
+echo off
+SET success=0
+SET error=1
+SET warning=2
+SET status=%success%
+
+SET log_path="c:\temp/preScript.log"
+echo Logs: > %log_path%
+
+rem %Service_Console have to be enabled and accept unauthorized login or O/S login
 C:\InterSystems\IRIS\bin\iris runw iris FREEZE^^ZBACKUP %%SYS
+SET sts=%errorlevel%
+if %sts%==0 goto OK
+if %sts%==1 goto FAIL
+:OK
+echo SYSTEM IS FROZEN >> %log_path%
+SET status=%success%
+goto END
+:FAIL
+echo SYSTEM FREEZE FAILED >> %log_path%
+SET status=%error%
+C:\InterSystems\IRIS\bin\iris runw iris THAW^^ZBACKUP %%SYS
+:END
+
+exit/B %status%
